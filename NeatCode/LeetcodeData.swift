40f52e5dbf41def9b30215f4c0afb34a50ff1d88
Ayush Rajpal <ayushrajpal2021@gmail.com>
2024-06-02 18:08:01 +0530

    fixing the API fetching and interface

---
+++ NeatCode/LeetcodeData.swift
@@ -0,0 +1,8 @@
+//
+//  LeetcodeData.swift
+//  NeatCode
+//
+//  Created by Ayush Rajpal on 02/06/24.
+//
+
+import Foundation
---
+++ NeatCode/LeetcodeStructureModel.swift
@@ -0,0 +1,8 @@
+//
+//  LeetcodeStructureModel.swift
+//  NeatCode
+//
+//  Created by Ayush Rajpal on 02/06/24.
+//
+
+import Foundation
